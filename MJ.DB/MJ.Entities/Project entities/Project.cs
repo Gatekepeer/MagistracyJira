3681c971ea0b0c8879dcddb454c50f82b309e598
kl\ext_ksionzhyk <ext_ksionzhyk@kaspersky.com>
2017-07-13 13:55:34 +0300

    Added class for project

---
+++ MJ.DB/MJ.Entities/Project entities/Project.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MJ.Entities.Project_entities
+{
+	public class Project : BaseEntity
+	{
+	}
+}
