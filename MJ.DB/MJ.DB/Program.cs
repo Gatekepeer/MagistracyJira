33fe4e56922cdd2edfbf3a0b445483bb02dd1781
kl\ext_ksionzhyk <ext_ksionzhyk@kaspersky.com>
2017-07-13 13:48:42 +0300

    Master. First commit

---
+++ MJ.DB/MJ.DB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MJ.DB
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Holly shit. I`ts worked");
+		}
+	}
+}
